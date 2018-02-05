66887e3d8a77a3ab014680bb0d7f3b127d4def0a
John Forster <j_forster@hotmail.co.uk>
2018-02-05 11:50:18 +0000

    Added something else.

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -6,7 +6,8 @@ public class MyScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("This is the Test Script")
+		Debug.Log("This is the Test Script");
+		Debug.Log("Added something else");
 	}
 	
 	// Update is called once per frame
